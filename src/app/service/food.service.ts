7fbc18bc1b2f48be04734ba5b2b08a8448b4c77d
imdeepakk <deepakmishra3813@gmail.com>
2024-11-07 14:18:19 +0530

    clean up code

--- src/app/service/food.service.ts
+++ src/app/service/food.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Food } from '../models/food';
 
 @Injectable({
   providedIn: 'root',
